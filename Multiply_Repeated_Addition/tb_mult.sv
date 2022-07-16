5b5bfc04111853749778ccee8b929ce21f74d11d
Abhishek Vashist <avashist003@gmail.com>
2022-07-16 12:36:34 -0400

    Update tb_mult.sv

--- Multiply_Repeated_Addition/tb_mult.sv
+++ Multiply_Repeated_Addition/tb_mult.sv
@@ -37,5 +37,39 @@ module test;
   // 50 MHz clock
   always #20 clk = ~clk;
   
+  // -------- SVA ------------------//
+  
+  logic [7:0] ref_mult_out;
+  logic [3:0] in_1, in_2;
+  
+  always_ff@(posedge clk, negedge rst_n) begin
+    if (!rst_n) begin
+      in_1 <= '0;
+      in_2 <= '0;
+    end
+    else if (valid_in && ready_out) begin
+      in_1 = in_a;
+      in_2 = in_b;
+    end
+  end
+  // reference output
+  assign ref_mult_out = in_1 * in_2;
+  
+  property out_check_P;
+    @(posedge clk) disable iff (!rst_n)
+    valid_out |-> (mult_out == ref_mult_out);
+  endproperty
+  
+  property valid_ready_in_P;
+    @(posedge clk) disable iff (!rst_n)
+    valid_in && !ready_out |-> ##1 valid_in && $stable(in_a) && $stable(in_b);
+  endproperty
+  
+  assume property(valid_ready_in_P) $display($time, " PASS ready/valid");
+    else $display($time, " FAILED ready/valid");
+  
+    assert property(out_check_P) $display($time, " PASS out check");
+      else $display($time, " FAILED out check %h %h", mult_out, ref_mult_out);
+  
 
 endmodule
